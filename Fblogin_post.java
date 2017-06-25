814b1295d1d578750760e90a2697491092552a1c
SolomonBlack <Soltbadguy@hotmail.com>
2017-06-24 18:50:58 -0700

    Adding 3 Java scripts
    Added 3 scripts based on the java programming language

---
+++ Fblogin_post.java
@@ -0,0 +1,13 @@
+/*
+
+The idea for this script to log into facebook and make a particular
+ post about something
+
+Steps for this include:
+1. Bringing up the browser
+2. locating elements
+3. typing in words in word boxes
+4. Pressing buttons on webpage
+5. typing in a status of some kind
+
+ */
---
+++ atm.java
@@ -0,0 +1,17 @@
+/*
+
+The idea for this script is to simulate a typical moment
+when you arrive at an atm
+
+Steps for this include:
+1. Arriving at the atm as someone with a card
+2. Inserting card
+3. Having the atm ask for a pin number
+4. Entering the pin number
+5. Having a series of options to choose from and choosing one
+6. picking a number based off that choice
+7. changing the persons account based off that number
+8. Giving the user the option to quit or star over
+9. end script
+
+*/

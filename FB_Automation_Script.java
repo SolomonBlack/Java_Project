4978506eea2bd27a2c5c40b79d824e6197a9cfe8
SolomonBlack <Soltbadguy@hotmail.com>
2017-06-27 22:22:14 -0700

    Updated automation script
    Chipped away at all the thread sleeps and added the timeunit module

--- FB_Automation_Script.java
+++ FB_Automation_Script.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class FB_Automation_Script{
 
@@ -12,10 +13,7 @@ public static void Facebooklogin() throws InterruptedException {
 	// Optional, if not specified, WebDriver will search your path for chromedriver.
     WebDriver driver = new ChromeDriver();
     driver.get("http://www.facebook.com");
-    // Let the user actually see something!
-    Thread.sleep(5000);
-    
-    System.out.println("Please type in your username and password");
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     
     //Ask any sort of person to input text with user email
     System.out.println("Please type in your username");
@@ -31,18 +29,19 @@ public static void Facebooklogin() throws InterruptedException {
     System.out.println("Typing in Username");
     WebElement email_box = driver.findElement(By.id("email"));
     email_box.sendKeys(Username);
-    Thread.sleep(5000);
     
     //finds user password box then Types in password based on user input
     System.out.println("Typing in password");
     WebElement pass_box = driver.findElement(By.id("pass"));
     pass_box.sendKeys(Password);
-    Thread.sleep(5000);
     
     //Presses the log in button and then bring user to homepage
     System.out.println("Loggin in");
     WebElement log_in_button = driver.findElement(By.id("loginbutton"));
     log_in_button.click();
+    
+    //Closes browser at the end of test
+    driver.close();
  }
 	
   public static void main(String[] args) throws InterruptedException {
